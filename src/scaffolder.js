42235f0050e072865a9a248261481e50e28be54c
Matt Travi <programmer@travi.org>
2020-06-05 23:36:06 -0500

    feat(script): defined the unit-test script

--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,3 +1,6 @@
 export default function () {
-  return {devDependencies: ['jest', 'jest-when']};
+  return {
+    devDependencies: ['jest', 'jest-when'],
+    scripts: {'test:unit:base': 'DEBUG=any jest --testPathPattern=src/.*\\.test\\.js$'}
+  };
 }
--- src/scaffolder.test.js
+++ src/scaffolder.test.js
@@ -2,6 +2,9 @@ import scaffold from './scaffolder';
 
 describe('scaffolder', () => {
   it('scaffolds jest', async () => {
-    expect(await scaffold()).toEqual({devDependencies: ['jest', 'jest-when']});
+    expect(await scaffold()).toEqual({
+      devDependencies: ['jest', 'jest-when'],
+      scripts: {'test:unit:base': 'DEBUG=any jest --testPathPattern=src/.*\\.test\\.js$'}
+    });
   });
 });
